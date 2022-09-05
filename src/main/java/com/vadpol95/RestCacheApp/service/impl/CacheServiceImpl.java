61efabcf591115387bb398ba0ad199697a9f51ef
VadPol95 <vpolyvyanoy@gmail.com>
2022-09-05 13:59:12 +0300

    Cache

--- src/main/java/com/vadpol95/RestCacheApp/service/impl/CacheServiceImpl.java
+++ src/main/java/com/vadpol95/RestCacheApp/service/impl/CacheServiceImpl.java
@@ -16,29 +16,31 @@ public class CacheServiceImpl implements CacheService {
     public void createCache(String cache) {
         cacheDAO.createCache(cache);
     }
+
     @Override
     public boolean put(String cache, String key, String str) {
-        cacheDAO.put(cache,key,str);
+        cacheDAO.put(cache, key, str);
         return true;
     }
 
     @Override
     public String get(String cache, String key) {
-        return cacheDAO.get(cache,key);
+        return cacheDAO.get(cache, key);
     }
 
     @Override
     public void clear() {
+        cacheDAO.clear();
 
     }
 
     @Override
     public void clear(String cache) {
-
+        cacheDAO.clear(cache);
     }
 
     @Override
     public void isCacheExist(String cache) {
-
+        cacheDAO.isCacheExist(cache);
     }
 }
